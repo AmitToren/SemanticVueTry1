bc12a2c07eb86edd97b4b307f9b74fb424c3cdeb
AmitToren <amit.toren12@gmail.com>
2018-08-16 15:21:09 +0300

    add
    add

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import Sem from "../src/SemanticTry.vue";
 import SuiVue from "semantic-ui-vue";
 
+import Sem from "../src/SemanticTry.vue";
+import VerticallyDividedExample from "../src/semanticForm.vue";
+
 Vue.config.productionTip = false;
+
 Vue.use(SuiVue);
+Vue.use(VerticallyDividedExample);
+
 /* eslint-disable no-new */
 new Vue({
   el: "#sem",
   components: { Sem },
   template: "<sem/>"
+});
+
+new Vue({
+  el: "#VerticallyDividedExample",
+  components: { VerticallyDividedExample },
+  template: "<VerticallyDividedExample/>"
 });
---
+++ src/semanticForm.vue
@@ -0,0 +1,66 @@
+<template lang="html">  
+  Curent Tab: {{curTab}}
+  <div is="sui-container">
+    <sui-menu :widths="3">
+      <sui-menu-item class="clickable">Editorials</sui-menu-item>
+      <sui-menu-item class="clickable">Reviews</sui-menu-item>
+      <sui-menu-item active class="clickable">Upcoming Events</sui-menu-item>
+    </sui-menu>
+  <sui-grid celled>
+    <sui-grid-row>
+      <sui-grid-column :width="3">
+        <sui-segment>
+            <h5>Information</h5>
+          </sui-segment>
+      </sui-grid-column>
+      <sui-grid-column :width="13">
+        <sui-form>
+    <sui-form-field>
+      <label>First Name</label>
+      <input placeholder="First Name" />
+    </sui-form-field>
+    <sui-form-field>
+      <label>Last Name</label>
+      <input placeholder="Last Name" />
+    </sui-form-field>
+    <sui-form-field>
+      <sui-checkbox label="I agree to the Terms and Conditions" />
+    </sui-form-field>
+    <sui-button type="submit">Submit</sui-button>
+  </sui-form>
+      </sui-grid-column>
+    </sui-grid-row>
+    <sui-grid-row>
+      <sui-grid-column :width="3">
+        <sui-segment>
+            <h5>Image</h5>
+          </sui-segment>
+      </sui-grid-column>
+      <sui-grid-column :width="10">
+        <sui-image src="http://lorempixel.com/400/200/" size="small" />
+      </sui-grid-column>
+      <sui-grid-column :width="3">
+        <div>
+    <sui-button primary compact>Accept</sui-button>
+    <sui-button secondary compact>Decline</sui-button>
+  </div>
+      </sui-grid-column>
+    </sui-grid-row>
+  </sui-grid>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VerticallyDividedExample",
+  props: {
+    curTab: "This"
+  }
+};
+</script>
+
+<style>
+  .clickable {
+    cursor: pointer;
+  }
+</style>
